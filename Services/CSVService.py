836f7612e5505d1ac1afa2a6a48952d06c082265
Dylano van der Wal <dylanovanderwal@gmail.com>
2025-10-04 20:20:18 +0200

    Retrieve Category IDs based on time

--- Services/CSVService.py
+++ Services/CSVService.py
@@ -19,15 +19,3 @@ def Name_To_Id(search_term=""):
             if row['Category Name'] == search_term:
                 return row['Category ID']
     return None
-
-
-def Name_To_IdTest():
-    print(Name_To_Id("Escape Room"))
-
-def Id_To_NameTest():
-    print(Id_To_Name("5f2c2834b6d05514c704451e"))
-
-
-if __name__ == "__main__":
-    Id_To_NameTest()
-    Name_To_IdTest()

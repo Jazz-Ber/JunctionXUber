db96f7863a858242a4a6bba5ce1572dfd2ac9087
Jazz Berends <j.j.berends@student.tudelft.nl>
2025-10-04 13:58:53 +0200

    Added logic between scripts

--- controller.py
+++ controller.py
@@ -1,8 +1,16 @@
 # The main controller script
 
-from ui import App
 import tkinter as tk
 
+class Controller:
+    def busy_address(self):
+        return "Den Haag"
+    
+    def idle_address(self):
+        return "Barendrecht"
+
 if __name__ == "__main__":
+    # Import here to avoid circular import
+    from ui import App
     app = App()
     app.start()

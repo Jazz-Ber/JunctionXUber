1936ef0b7211f1c2f7f9e4396f6d1f4789b8c953
Nils <nilsvanelswijk@gmail.com>
2025-10-04 12:22:20 +0200

    http_client

---
+++ http_client.py
@@ -0,0 +1,11 @@
+import requests
+
+url = ""
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+else:
+    print("Request failed:", response.status_code)
